c8d5436c2a36bac1612139581f88f163557a2395
MJeorrett <matthew@clearskylogic.com>
2022-03-18 12:44:15 +0000

    Add some summaries

--- GitDemo.WebApi/Controllers/WeatherForecastController.cs
+++ GitDemo.WebApi/Controllers/WeatherForecastController.cs
@@ -8,8 +8,20 @@ namespace GitDemo.WebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", 
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching",
+            "Taps aff",
+            "Taps on",
+            "Guns out"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
